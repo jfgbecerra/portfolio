23e82152366d80a76992bb2d4fb8c0c3a842325b
Jose Fernando Garcia Becerra <josefernandogarcia76@gmail.com>
2024-02-07 16:53:29 -0600

    Update footer to use next link fix logo flicker

--- src/components/Buttons/FooterLink.tsx
+++ src/components/Buttons/FooterLink.tsx
@@ -1,18 +1,20 @@
+import Link from 'next/link';
+
 type FooterLinkProps = {
   name: string;
 
   href: string;
 };
 
-export default function FooterLink({name, href}: FooterLinkProps) {
+export default function FooterLink({ name, href }: FooterLinkProps) {
   return (
     <div key={name} className='pb-6'>
-      <a
+      <Link
         href={href}
         className='text-sm leading-6 text-gray-600 hover:text-gray-900'
       >
         {name}
-      </a>
+      </Link>
     </div>
   );
 }
