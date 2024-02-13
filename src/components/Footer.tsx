bf42a3695c3d99e1c3b8eafe8d859646e70ffddb
Jose Fernando Garcia Becerra <josefernandogarcia76@gmail.com>
2024-02-12 20:50:15 -0600

    Fix two column alignment bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -129,7 +129,7 @@ export default function Footer() {
     <footer className='bg-white'>
       <div className='mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-10 lg:px-8'>
         <nav
-          className='-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12'
+          className='-mb-6 flex h-28 flex-col flex-wrap items-center sm:h-12 sm:flex-row sm:justify-center sm:space-x-12'
           aria-label='Footer'
         >
           {navigation.main.map((item) => (
